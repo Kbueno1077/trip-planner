e24ea86f99c89000916efe5a18a1e5788a1ace46
Kbueno1077 <kbueno1077@gmail.com>
2025-08-17 17:25:24 -0400

    Refactor Arcjet API integration in route.ts
    - Removed the Arcjet API route implementation, including bot detection and rate limiting logic, to simplify the codebase.
    - Retained the import of the Arcjet configuration for future use while streamlining the GET request handling.

--- app/api/arcjet/route.ts
+++ app/api/arcjet/route.ts
@@ -1,37 +1,6 @@
+import { aj } from "@/arcject/config";
 import { isSpoofedBot } from "@arcjet/inspect";
-import arcjet, { detectBot, shield, tokenBucket } from "@arcjet/next";
 import { NextResponse } from "next/server";
-
-export const aj = arcjet({
-  key: process.env.ARCJET_KEY!, // Get your site key from https://app.arcjet.com
-  rules: [
-    // Shield protects your app from common attacks e.g. SQL injection
-    shield({ mode: "LIVE" }),
-    // Create a bot detection rule
-    detectBot({
-      mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
-      // Block all bots except the following
-      allow: [
-        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
-        // Uncomment to allow these other common bot categories
-        // See the full list at https://arcjet.com/bot-list
-        //"CATEGORY:MONITOR", // Uptime monitoring services
-        //"CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
-      ],
-    }),
-    // Create a token bucket rate limit. Other algorithms are supported.
-    tokenBucket({
-      mode: "LIVE",
-      characteristics: ["userId"],
-      // Tracked by IP address by default, but this can be customized
-      // See https://docs.arcjet.com/fingerprints
-      //characteristics: ["ip.src"],
-      refillRate: 10, // Refill 5 tokens per interval
-      interval: "1d", // Refill every day
-      capacity: 10, // Bucket capacity of 10 tokens
-    }),
-  ],
-});
 
 export async function GET(req: Request) {
   const decision = await aj.protect(req, { requested: 5, userId: "123" }); // Deduct 5 tokens from the bucket
